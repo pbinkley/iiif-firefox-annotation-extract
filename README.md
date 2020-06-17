208273b6af62cde639f6a43fa543c4d15e4c65ba
Peter Binkley <peter.binkley@ualberta.ca>
2020-06-17 09:33:57 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # iiif-firefox-annotation-extract
 Ruby script to extract Mirador-created annotations from Firefox local storage
+
+To install: clone this repo and run ```bundle install```
+
+To run: 
+
+- Create some annotations using Mirador in Firefox
+- Discover your current Firefox profile directory by visiting ```about:profiles```. Set an environment variable ```FIREFOX_PROFILE``` containing the full path (ending with ```.default```)
+- Run ```./extract-annotations.rb <canvasURI selector> [<scope>]```
+  - the canvasURI selector is the beginning of the uri for the canvases you're interested in
+  - the scope is the domain of the Mirador instance you used to make the annotations (default: ```projectmirador.org```)
+- An annotation list is written to stdout
---
+++ extract-annotations.rb
@@ -0,0 +1,53 @@
+#!/usr/bin/env ruby
+
+require 'sqlite3'
+require 'json'
+require 'active_support/core_ext/digest/uuid'
+require 'tmpdir'
+
+DBFILENAME = 'webappsstore.sqlite'.freeze
+
+canvasPrefix = ARGV[0] # as much of the canvas uri pattern as you need to select your annotations
+scope = (ARGV[1] || 'projectmirador.org').reverse
+
+firefox = ENV['FIREFOX_PROFILE'] # full path, like '/home/pbinkley/.mozilla/firefox/kkfnzolx.default'
+
+# copy live db to working location, to unlock it
+# note: mine is 148mb
+
+tempDB = "#{Dir.tmpdir()}/#{DBFILENAME}"
+
+FileUtils.cp("#{firefox}/#{DBFILENAME}", tempDB)
+
+db = SQLite3::Database.new(tempDB)
+query = "SELECT value FROM webappsstore2 where scope like \"#{scope}%\" and key like \"#{canvasPrefix}%\";"
+annotations = []
+
+db.execute( query ) do |row|
+  row.each do |val|
+    JSON.parse(val).each do |item|
+      # add region fragment to end of canvas URI to create annotation URI
+      oldOn = item['on'].first
+      newOn = "#{ oldOn['full'] }##{ oldOn['selector']['default']['value'] }"
+      item['on'] = newOn
+      item['motivation'] = 'oa:painting'
+      annotations << item
+    end
+  end
+end
+
+db.close
+
+# convert into simple list, modelled on https://github.com/glenrobson/SimpleAnnotationServer/blob/master/src/main/webapp/examples/anno_list.json
+
+list = {
+    "@context": "http://iiif.io/api/presentation/2/context.json",
+    "@id": Digest::UUID.uuid_v4,
+    "@type": "sc:AnnotationList",
+    "resources": annotations
+  }
+
+puts JSON.pretty_generate(list)
+
+# don't leave the temp copy lying around
+FileUtils.rm(tempDB)
